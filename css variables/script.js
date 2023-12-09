8d6d808ec295c7ff9d8936d749a7dc51a2c6f04b
frecklehead <prashamsa35np@gmail.com>
2023-12-09 12:28:30 +0545

    `

--- css variables/script.js
+++ css variables/script.js
@@ -1,8 +1,10 @@
+
 const inputs= document.querySelectorAll("input")
- function change (){
-const suffix= this.dataset.sizing || "";
-document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
- }
+function change(){
 
+ const suffix=this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
 
-inputs.forEach(input=> input.addEventListener("change", change))
+}
+
+inputs.forEach((input)=> input.addEventListener("change",change ))
